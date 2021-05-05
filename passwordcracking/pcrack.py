d6965cad973ed02ac73777ab133e03fc83d97e3e
PeterCarletonCollege <mccreap2@carleton.edu>
2021-05-05 17:45:36 -0500

    sfgfg

--- passwordcracking/pcrack.py
+++ passwordcracking/pcrack.py
@@ -1,10 +1,50 @@
 import hashlib
 import itertools 
-wordsRaw = [line.strip().lower() for line in open('words.txt')]
-outList = []
-for i in wordsRaw:
-    for j in wordsRaw:
-        outList.append(i+j)
+import binascii
+import sys
+from math import floor
+from time import time
+from tqdm import tqdm
 
 
-print(outList)
+def compute_hash(word):
+    encoded = word.encode('utf-8')
+    md5 = hashlib.md5(encoded)
+    word_hash = md5.digest()
+    hash_hex = binascii.hexlify(word_hash)
+    hash_hex_string = hash_hex.decode('utf-8')
+    return hash_hex_string
+
+words = [line.strip().lower() for line in open('words.txt')]
+
+user_hash = dict()
+for line in open('passwords1.txt'):
+    line = line.split(":")
+    user_hash[line[1]] = line[0]
+
+hashes_computed = 0
+start = time()
+
+
+for i in range(len(words)):
+    word = words[i]
+    word_hash = compute_hash(word)
+    hashes_computed += 1
+
+    if word_hash in user_hash:
+        with open('cracked.txt', "a") as f:
+            f.write("{0}:{1}\n".format(user_hash[word_hash], word))
+
+    for second_word in words:
+        pair = word + second_word
+        pair_hash = compute_hash(pair)
+        hashes_computed += 1
+
+        if pair_hash in user_hash:
+            with open('cracked.txt', "a") as f:
+                f.write("{0}:{1}\n".format(user_hash[pair_hash], pair))
+
+
+end = time()
+with open('timings.txt', "w") as f:
+    f.write(str(hashes_computed) + " " + str(end - start) + "\n")
